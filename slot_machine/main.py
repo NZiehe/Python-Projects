4e7cda511cd2fc640396cf5589e762ac86fc28d2
Nico Ziehe <nico.ziehe@outlook.com>
2024-03-16 14:21:17 +0100

    added get_bet()
    get_bet() lets the user set his bet for each line between a MAX and MIN value

--- slot_machine/main.py
+++ slot_machine/main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 5000
+MIN_BET = 10
 
 # function gets a deposit as input from the user
 def deposit():
@@ -14,7 +16,7 @@ def deposit():
             print("Please enter a number!")
     return amount
 
-#
+# get the number on lines the user want to bet on
 def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?: ")
@@ -28,10 +30,28 @@ def get_number_of_lines():
             print("Please enter a number!")
     return lines
 
+# get the amount of money he want to bet
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? MAX: " + str(MAX_BET) + "$, MIN: " + str(MIN_BET) + "$ : ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print("Bet must be between MAX: " + str(MAX_BET) + "$, MIN: " + str(MIN_BET) + "$ : ")
+
+        else:
+            print("Please enter a number!")
+
+    return amount
+
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance + lines)
+    bet = get_bet()
+
+    print(balance, lines)
 
 main()
