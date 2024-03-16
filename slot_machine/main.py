24e5e5bf46a41113bd355a58d4b9866f44a8b91b
Nico Ziehe <nico.ziehe@outlook.com>
2024-03-16 14:11:24 +0100

    added get_number_of_lines
    user gets to choose between 1  and MAX_LINES of lines to choose

--- slot_machine/main.py
+++ slot_machine/main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 # function gets a deposit as input from the user
 def deposit():
     while True:
@@ -12,4 +14,24 @@ def deposit():
             print("Please enter a number!")
     return amount
 
-deposit()
+#
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?: ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("enter a valid number of lines!")
+        else:
+            print("Please enter a number!")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance + lines)
+
+main()
