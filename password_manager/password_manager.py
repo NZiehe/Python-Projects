8a566a122ebed80eae9ac84dacfe8ed44eeb7907
Nico Ziehe <nico.ziehe@outlook.com>
2024-03-16 13:03:10 +0100

    created add function

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,12 +1,26 @@
 # !! not a secure way to store passwords!!
 
-pwd = input("What is the master password?: ")
+master_pwd = input("What is the master password?: ")
+
+def view():
+   pass
+
+def add():
+   name = input("Account Name: ")
+   pwd = input("Password: ")
+
+   with open("passwords.txt", "a") as f:
+       f.write(name + "|" + pwd + "\n")
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones (add/view)?:  ").lower()
-    
-if mode == "view":
-    pass
-elif mode == "add":
-    pass
-else:
+    mode = input("Would you like to add a new password or view existing ones (add/view)?, press q to quit!:  ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        continue
